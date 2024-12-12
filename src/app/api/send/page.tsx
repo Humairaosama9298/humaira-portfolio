5250058ce28bed20f534503b701b102b1352942c
Humairaosama9298 <faryalfilza@gmail.com>
2024-12-12 22:09:35 +0500

    updated

--- src/app/api/send/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export default async function Get() {
-    try {
-      return NextResponse.json({ 'hello': 'world'});
-    } catch (error) {
-        return NextResponse.json({error});
-    }
-}
